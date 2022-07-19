87bba46bf44c87a8d5b9b64eb3ff19f58d8bdbac
yf <1446406982@qq.com>
2022-07-19 18:54:58 +0800

    个人资料页面设置跳转参数

--- app/src/main/java/com/twx/marryfriend/friend/FriendInfoActivity.kt
+++ app/src/main/java/com/twx/marryfriend/friend/FriendInfoActivity.kt
@@ -1,6 +1,14 @@
 package com.twx.marryfriend.friend
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 
 class FriendInfoActivity:AppCompatActivity() {
+    companion object{
+        fun getIntent(context: Context,userId:Int):Intent{
+            val intent=Intent(context,FriendInfoActivity::class.java)
+            return intent
+        }
+    }
 }
