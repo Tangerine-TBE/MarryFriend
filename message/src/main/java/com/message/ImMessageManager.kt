431623120102ede45cadb9d18fd4b9f4e99d83c3
‘guocai’ <2625613473@qq.com>
2022-09-28 22:32:17 +0800

    9/28

--- message/src/main/java/com/message/ImMessageManager.kt
+++ message/src/main/java/com/message/ImMessageManager.kt
@@ -21,15 +21,18 @@ import com.xyzz.myutils.show.iLog
 //http://sdkdocs.easemob.com/apidoc/android/chat3.0/annotated.html
 object ImMessageManager {
 
-    val newMessageLiveData=MutableLiveData<List<Message<out EMMessageBody>>?>()//收到新消息
-    private val fromConversationRead=MutableLiveData<String>()
-    private val messageRead=MutableLiveData<List<EMMessage>>()
+    val newMessageLiveData = MutableLiveData<List<Message<out EMMessageBody>>?>()//收到新消息
+    private val fromConversationRead = MutableLiveData<String>()
+    private val messageRead = MutableLiveData<List<EMMessage>>()
 
-    private val messageDelivered=MutableLiveData< MutableList<EMMessage>?>()
+    private val messageDelivered = MutableLiveData<MutableList<EMMessage>?>()
 
-    fun observeNewMessage(owner: LifecycleOwner,observer: Observer<List<Message<out EMMessageBody>>?>){
-        newMessageLiveData.value=null
-        newMessageLiveData.observe(owner,observer)
+    fun observeNewMessage(
+        owner: LifecycleOwner,
+        observer: Observer<List<Message<out EMMessageBody>>?>,
+    ) {
+        newMessageLiveData.value = null
+        newMessageLiveData.observe(owner, observer)
     }
 
     private val msgListener by lazy {
@@ -37,126 +40,133 @@ object ImMessageManager {
             override fun onMessageReceived(messages: MutableList<EMMessage>?) {
                 //收到消息
 
-                Log.i("guo","收到消息1111")
+                Log.i("guo", "收到消息1111")
 
-                iLog(messages?.firstOrNull()?.from,"收到消息,收到")
+                iLog(messages?.firstOrNull()?.from, "收到消息,收到")
                 newMessageLiveData.postValue(messages?.mapNotNull { Message.toMyMessage(it) })
             }
 
             override fun onCmdMessageReceived(messages: MutableList<EMMessage>?) {
                 //收到透传消息
 
-                Log.i("guo","收到透传消息")
+                Log.i("guo", "收到透传消息")
 
-                iLog(messages?.firstOrNull()?.from,"收到消息,cmd")
+                iLog(messages?.firstOrNull()?.from, "收到消息,cmd")
             }
 
             override fun onMessageRead(messages: MutableList<EMMessage>?) {
                 //收到已读回执
 
-                Log.i("guo","收到已读回执")
-                iLog("收到消息,已读"+messages?.firstOrNull()?.from+"发给"+messages?.firstOrNull()?.to)
-                messageRead.value=messages
+                Log.i("guo", "收到已读回执")
+                iLog("收到消息,已读" + messages?.firstOrNull()?.from + "发给" + messages?.firstOrNull()?.to)
+                messageRead.value = messages
             }
 
             override fun onMessageDelivered(messages: MutableList<EMMessage>?) {
                 //收到已送达回执
 
-                Log.i("guo","收到已送达回执")
-                iLog(messages?.firstOrNull()?.from,"收到消息,已送达")
-                messageDelivered.value=messages
+                Log.i("guo", "收到已送达回执")
+                iLog(messages?.firstOrNull()?.from, "收到消息,已送达")
+                messageDelivered.value = messages
             }
 
             override fun onMessageRecalled(messages: MutableList<EMMessage>?) {
                 //消息状态变动
 
-                Log.i("guo","消息状态变动")
-                iLog(messages?.firstOrNull()?.from,"收到消息,状态")
+                Log.i("guo", "消息状态变动")
+                iLog(messages?.firstOrNull()?.from, "收到消息,状态")
             }
-
 
 
         }
     }
 
     private val conversationsListener by lazy {
-        object : EMConversationListener{
+        object : EMConversationListener {
             override fun onCoversationUpdate() {
-                
+
             }
 
             override fun onConversationRead(from: String?, to: String?) {
-                iLog("会话已读${from+"发送给"+to}")
-                fromConversationRead.value=from
+                iLog("会话已读${from + "发送给" + to}")
+                fromConversationRead.value = from
             }
         }
     }
 
-    fun getAllConversations():List<ConversationsBean>{
+    fun getAllConversations(): List<ConversationsBean> {
         val conversations = EMClient.getInstance().chatManager().allConversations
-        val conversationsBeanList=ArrayList<ConversationsBean>()
+        val conversationsBeanList = ArrayList<ConversationsBean>()
         conversations.keys.forEach {
-            val conversation=conversations[it]
+            val conversation = conversations[it]
             ConversationsBean(it).apply {
                 conversationsBeanList.add(this)
-                this.unReaderCount=conversation?.unreadMsgCount?:0
-                val lastMsg=conversation?.lastMessage?:return@apply
-                this.lastTime= lastMsg.msgTime
-                this.lastMassage=
-                when(lastMsg.type){
-                    EMMessage.Type.TXT -> {
-                        (lastMsg.body as? EMTextMessageBody)?.message
+                this.unReaderCount = conversation?.unreadMsgCount ?: 0
+                val lastMsg = conversation?.lastMessage ?: return@apply
+                this.lastTime = lastMsg.msgTime
+                this.lastMassage =
+                    when (lastMsg.type) {
+                        EMMessage.Type.TXT -> {
+                            (lastMsg.body as? EMTextMessageBody)?.message
+                        }
+                        EMMessage.Type.IMAGE -> {
+                            "[图片]"
+                        }
+                        EMMessage.Type.VIDEO -> {
+                            "[视频]"
+                        }
+                        EMMessage.Type.LOCATION -> {
+                            "[位置]"
+                        }
+                        EMMessage.Type.VOICE -> {
+                            "[语音]"
+                        }
+                        EMMessage.Type.FILE -> {
+                            "[文件]"
+                        }
+                        EMMessage.Type.CMD -> {
+                            "[透传]"
+                        }
+                        EMMessage.Type.CUSTOM -> {
+                            "用户自定义消息"
+                        }
+                        null -> {
+                            null
+                        }
                     }
-                    EMMessage.Type.IMAGE -> {
-                        "[图片]"
-                    }
-                    EMMessage.Type.VIDEO -> {
-                        "[视频]"
-                    }
-                    EMMessage.Type.LOCATION -> {
-                        "[位置]"
-                    }
-                    EMMessage.Type.VOICE -> {
-                        "[语音]"
-                    }
-                    EMMessage.Type.FILE -> {
-                        "[文件]"
-                    }
-                    EMMessage.Type.CMD -> {
-                        "[透传]"
-                    }
-                    EMMessage.Type.CUSTOM -> {
-                        "用户自定义消息"
-                    }
-                    null -> {
-                        null
-                    }
-                }
-                this.conversationType= ConversationType.toMyType(conversation.type) ?:throw IllegalStateException("还未实现该会话")
+                this.conversationType = ConversationType.toMyType(conversation.type)
+                    ?: throw IllegalStateException("还未实现该会话")
             }
         }
         return conversationsBeanList
     }
 
-    fun getHistoryMessage(toChatUsername: String, pageSize: Int, msgId: String? = null):List<Message<out EMMessageBody>>{
-        val resultList=ArrayList<EMMessage>()
+    fun getHistoryMessage(
+        toChatUsername: String,
+        pageSize: Int,
+        msgId: String? = null,
+    ): List<Message<out EMMessageBody>> {
+        val resultList = ArrayList<EMMessage>()
         try {
-            val conversation = EMClient.getInstance().chatManager().getConversation(toChatUsername)?:return emptyList()
+            val conversation = EMClient.getInstance().chatManager().getConversation(toChatUsername)
+                ?: return emptyList()
             val ramMsg = conversation.allMessages.asReversed()
-            if (msgId!=null&&msgId.isNotBlank()){
-                val index=ramMsg.indexOfFirst {
-                    it.msgId==msgId
+            if (msgId != null && msgId.isNotBlank()) {
+                val index = ramMsg.indexOfFirst {
+                    it.msgId == msgId
                 }
-                if (index==-1){
+                if (index == -1) {
                     resultList.addAll(conversation.loadMoreMsgFromDB(msgId, pageSize))
-                }else{
-                    resultList.addAll(ramMsg.slice(index+1 until ramMsg.size))
-                    resultList.addAll(conversation.loadMoreMsgFromDB(ramMsg[index].msgId,pageSize-resultList.size))
+                } else {
+                    resultList.addAll(ramMsg.slice(index + 1 until ramMsg.size))
+                    resultList.addAll(conversation.loadMoreMsgFromDB(ramMsg[index].msgId,
+                        pageSize - resultList.size))
                 }
-            }else{
+            } else {
                 resultList.addAll(ramMsg)
-                if (ramMsg.size<pageSize){
-                    resultList.addAll(conversation.loadMoreMsgFromDB(ramMsg.lastOrNull()?.msgId,pageSize-ramMsg.size))
+                if (ramMsg.size < pageSize) {
+                    resultList.addAll(conversation.loadMoreMsgFromDB(ramMsg.lastOrNull()?.msgId,
+                        pageSize - ramMsg.size))
                 }
             }
         } catch (e: HyphenateException) {
@@ -169,11 +179,11 @@ object ImMessageManager {
         }
     }
 
-    fun getAllUnreadMessage():Int{
+    fun getAllUnreadMessage(): Int {
         return EMClient.getInstance().chatManager().unreadMessageCount
     }
 
-    fun markAllMsgAsRead(username: String){
+    fun markAllMsgAsRead(username: String) {
         val conversation = EMClient.getInstance().chatManager().getConversation(username)
         //指定会话消息未读数清零
         conversation.markAllMessagesAsRead()
@@ -183,7 +193,7 @@ object ImMessageManager {
 //        EMClient.getInstance().chatManager().markAllConversationsAsRead()
     }
 
-    fun ackConversationRead(conversationId:String){
+    fun ackConversationRead(conversationId: String) {
         try {
             EMClient.getInstance().chatManager().ackConversationRead(conversationId)
         } catch (e: HyphenateException) {
@@ -207,11 +217,11 @@ object ImMessageManager {
         }
     }
 
-    private fun updateMessage(msg:EMMessage){
+    private fun updateMessage(msg: EMMessage) {
         EMClient.getInstance().chatManager().updateMessage(msg)
     }
 
-    fun sendTextMsg(username: String,content:String):Message<out EMMessageBody>?{
+    fun sendTextMsg(username: String, content: String): Message<out EMMessageBody>? {
         iLog("发送\"${content}\"给${username}")
         //创建一条文本消息，content为消息文字内容，toChatUsername为对方用户或者群聊的id，后文皆是如此
         val message = EMMessage.createTxtSendMessage(content, username)
@@ -225,19 +235,19 @@ object ImMessageManager {
         return Message.toMyMessage(message)
     }
 
-    fun sendImageMsg(username: String,uri: Uri):Message<out EMMessageBody>?{
-        val message=EMMessage.createImageSendMessage(uri,true,username)
+    fun sendImageMsg(username: String, uri: Uri): Message<out EMMessageBody>? {
+        val message = EMMessage.createImageSendMessage(uri, true, username)
         EMClient.getInstance().chatManager().sendMessage(message)
         return Message.toMyMessage(message)
     }
 
-    fun sendImageMsg(username: String,file: String):Message<out EMMessageBody>?{
-        val message=EMMessage.createImageSendMessage(file,true,username)
+    fun sendImageMsg(username: String, file: String): Message<out EMMessageBody>? {
+        val message = EMMessage.createImageSendMessage(file, true, username)
         EMClient.getInstance().chatManager().sendMessage(message)
         return Message.toMyMessage(message)
     }
 
-    fun sendVoiceMsg(username: String,voiceUri:Uri,length:Int):Message<out EMMessageBody>?{
+    fun sendVoiceMsg(username: String, voiceUri: Uri, length: Int): Message<out EMMessageBody>? {
         //voiceUri 为语音文件本地资源标志符，length 为录音时间(秒)
         val message = EMMessage.createVoiceSendMessage(voiceUri, length, username)
 //如果是群聊，设置 chattype，默认是单聊
@@ -246,7 +256,7 @@ object ImMessageManager {
         return Message.toMyMessage(message)
     }
 
-    fun sendFlower(username: String):Message<out EMMessageBody>?{
+    fun sendFlower(username: String): Message<out EMMessageBody>? {
         val customMessage = EMMessage.createSendMessage(EMMessage.Type.CUSTOM)
 // event为需要传递的自定义消息事件，比如礼物消息，可以设置event = "gift"
         val customBody = EMCustomMessageBody(CustomMessage.CustomEvent.flower.code)
@@ -261,7 +271,7 @@ object ImMessageManager {
         return Message.toMyMessage(customMessage)
     }
 
-    fun getCustomMessage(username: String, type:CustomMessage.CustomEvent):EMMessage?{
+    fun getCustomMessage(username: String, type: CustomMessage.CustomEvent): EMMessage? {
         val customMessage = EMMessage.createSendMessage(EMMessage.Type.CUSTOM)
         val customBody = EMCustomMessageBody(type.code)
         customMessage.addBody(customBody)
@@ -270,18 +280,18 @@ object ImMessageManager {
         return customMessage
     }
 
-    fun insertMessage(message: EMMessage){
-        message.msgTime=System.currentTimeMillis()+50000
+    fun insertMessage(message: EMMessage) {
+        message.msgTime = System.currentTimeMillis() + 50000
         // 将消息插入到指定会话中。
         val conversation = EMClient.getInstance().chatManager().getConversation(message.to)
         conversation.insertMessage(message)
         // 直接插入消息。
-        message.isDelivered=true
+        message.isDelivered = true
         message.setStatus(EMMessage.Status.SUCCESS)
         EMClient.getInstance().chatManager().saveMessage(message)
     }
 
-    fun startMessageListener(){
+    fun startMessageListener() {
         iLog("用户${EMClient.getInstance().currentUser}注册消息监听")
         EMClient.getInstance().chatManager().removeMessageListener(msgListener)
         EMClient.getInstance().chatManager().addMessageListener(msgListener)
