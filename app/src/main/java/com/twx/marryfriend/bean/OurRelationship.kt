5527ecbac166bde4ba58b13ffbda0ce5c67730f6
yf <1446406982@qq.com>
2022-10-13 14:30:12 +0800

    bug

---
+++ app/src/main/java/com/twx/marryfriend/bean/OurRelationship.kt
@@ -0,0 +1,9 @@
+package com.twx.marryfriend.bean
+
+class OurRelationship{
+    var woPingBiTa=false
+    var woGuanZhuTa=false
+    var ta_zhuxiao=false
+
+    var ta_blacklist: InterdictionBean?=null
+}
